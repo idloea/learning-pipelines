9aece82124257284ba64eab2666e5678ca8a4f15
De Lomaosorio <IKER.DELOMAOSORIO@siemensgamesa.com>
2020-12-07 17:22:31 +0100

    Load data

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 """
-name: main.py
+project: learning-pipelines
 author: Iker De Loma-Osorio
 date: 06/12/2020
 
-This is the main script to use pipelines in Python
+This is the main script to use pipelines in Python.
+Data source: https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data?select=test.csv
 
 """
 
-import numpy as np
 import pandas as pd
 
-# Load data
-path = r"C:\Users\Usuario\Documents\00_Iker\Datasets\CreditCardFraudDetection_Kaggle"
-file = "creditcard.csv"
-df = pd.read_csv(path + "\\" + file)
+# Load train and test datasets
+path = r"C:\Users\iker.delomaosorio\Documents\Datasets\HousePricesAdvancedRegressionTechniques_Kaggle"
+
+file_train = "train.csv"
+file_test = "test.csv"
+
+train = pd.read_csv(path + "\\" + file_train)
+test = pd.read_csv(path + "\\" + file_test)
+
+
+# Check if there are missing values
+print("Missing values in train:", train.isnull().sum().sum())
+print("Missing values in test:", test.isnull().sum().sum())
+
+# Exploratory Data Analysis
+print(train.head())
+print(test.head())
 
