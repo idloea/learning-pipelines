49936bc7a5857c3c51fbde3de898c0625b482464
De Lomaosorio <IKER.DELOMAOSORIO@siemensgamesa.com>
2020-12-09 17:10:25 +0100

    Add models

--- main.py
+++ main.py
@@ -10,51 +10,52 @@ Resource: https://medium.com/vickdata/a-simple-guide-to-scikit-learn-pipelines-4
 """
 
 import pandas as pd
-import numpy as np
 from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.svm import SVC, NuSVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.svm import SVC
 
-# Load train and test datasets
-
+# Load the dataset
 path = r"C:\Users\iker.delomaosorio\Documents\Datasets\HousePricesAdvancedRegressionTechniques_Kaggle"
+file = "train.csv"
+data = pd.read_csv(path + "\\" + file)
 
-file_train = "train.csv"
-file_test = "test.csv"
+# Remove the Id column, since it does not add any value
+data = data.drop('Id', axis=1)
 
-train = pd.read_csv(path + "\\" + file_train)
-test = pd.read_csv(path + "\\" + file_test)
+# Separate the dataset into features (predictors) and target (value to predict)
+X = data.drop('SalePrice', axis=1)
+y = data.SalePrice
 
-# Remove the Id column
-train = train.drop('Id', axis=1)
+# Check the missing values
+print("Missing values in the features:", X.isnull().sum().sum())  # Some missing values
+print("Missing values in target:", y.isnull().sum())  # No missing values
 
-# Remove the target column (SalePrice) from the other columns
-X = train.drop('SalePrice', axis=1)
-y = train.SalePrice
-print("Missing values in the features:", X.isnull().sum().sum())
-print("Missing values in target:", y.isnull().sum())
-
-# Split the training dataset into train and test to later validate the performance of the models
+# Split the dataset into train and test to later validate the performance of the models
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Divide the features by type
-print("Unique data types in the train dataset:", train.dtypes.unique())
+# Check the type of features of the dataset
+print("Unique data types in the train dataset:", data.dtypes.unique())
 # There are numerical (int64 and float64) and categorical (object) types
-# Select the numeric features (without the target)
+
+# Select the numeric features. Do not consider the target,as it does not have any missing value
+# Thus, use X instead of data dataframe
 numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
-# Select the non-numeric (categorical) columns
+
+# Select the non-numeric (categorical) features
 categorical_features = X.select_dtypes(include=['object']).columns
 
-# Transformers
-numeric_transformer = SimpleImputer(strategy='constant')
+# The transformation will be made according to the data type of each feature
+# For the numeric features imputing the missing values is enough
+numeric_transformer = SimpleImputer(strategy='median')
 
+# For the categorical features besides of imputation, one-hot encoding needs to be performed since the models
+# only accept numerical data
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='most_frequent')),
     ('onehot', OneHotEncoder(handle_unknown='ignore'))
@@ -73,21 +74,28 @@ my_pipeline = Pipeline(steps=[
     ('preprocessor', preprocesor),
     ('model', model)])
 
-# Fit the pipeline
-my_pipeline.fit(X_train, y_train)
+# With more models
+models = [KNeighborsClassifier(3),
+          SVC(kernel="rbf", C=0.025, probability=True),
+          RandomForestClassifier(n_estimators=100, random_state=0)]
 
-# Predictions
-predictions = my_pipeline.predict(X_test)
+# Go through the chosen models
+score_list = []
+for model in models:
+    pipe = Pipeline(steps=[('preprocessor', preprocesor),
+                           ('model', model)])
+    pipe.fit(X_train, y_train)
+    predictions = pipe.predict(X_test)
+    score = mean_absolute_error(y_test, predictions)
+    score_list.append(score)
 
-# Evaluate the model
-score = mean_absolute_error(y_test, predictions)
-print("MAE:", score)
+# Create a list of the models used
+model_names = ['KNN', 'SVC', 'RandomForest']
 
+# Create a dataframe with the scores
+score_results = pd.DataFrame()
+score_results['Model'] = model_names
+score_results['MAE'] = score_list
 
-
-
-# "Cardinality" means the number of unique values in a column
-# Select categorical columns with relatively low cardinality (convenient but arbitrary)
-# categorical_cols = [cname for cname in X_train.columns if X_train[cname].nunique() < 10 and
-#                     X_train[cname].dtype == "object"]
-
+# Sort the MAE values from smallest to bigger to rank the models from best to worse
+print(score_results.sort_values(by='MAE'))
